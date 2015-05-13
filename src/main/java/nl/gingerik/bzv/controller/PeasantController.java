323883bfd23cbbb5fcbcee746753f1993fae6151
Erik Timmers <e.timmers@gmail.com>
2015-05-13 22:49:56 +0200

    Attempt to use Hibernate / ORM [WIP]

--- src/main/java/nl/gingerik/bzv/controller/PeasantController.java
+++ src/main/java/nl/gingerik/bzv/controller/PeasantController.java
@@ -3,27 +3,31 @@ package nl.gingerik.bzv.controller;
 import java.util.List;
 
 import nl.gingerik.bzv.model.Peasant;
-import nl.gingerik.bzv.repository.PeasantRepository;
 
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@ImportResource("classpath:applicationContext.xml")
+@Transactional
 public class PeasantController {
-
+	
     @Autowired
-    private PeasantRepository peasants;
+    private SessionFactory sessionFactory;
 
     @RequestMapping(value="/peasant", method=RequestMethod.GET)
     public List<Peasant> getList() {
-    	return peasants.getAll();
+		return (List<Peasant>) sessionFactory.getCurrentSession().createCriteria(Peasant.class).list();
     }
 
     @RequestMapping(value="/peasant/{peasantId}", method=RequestMethod.GET)
     public Peasant getById(@PathVariable("peasantId") long peasantId) {
-    	return peasants.getById(peasantId);
+    	return (Peasant) sessionFactory.getCurrentSession().load(Peasant.class, peasantId);
     }
 }
--- src/main/java/nl/gingerik/bzv/model/Peasant.java
+++ src/main/java/nl/gingerik/bzv/model/Peasant.java
@@ -1,15 +1,26 @@
 package nl.gingerik.bzv.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+
+@Entity
+@Table(name="bzv_peasants")
 public class Peasant {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
     private long id;
+	
     private int year;
     private String name;
     
-    public Peasant(long id, int year, String name) {
-    	this.id = id;
-    	this.year = year;
-    	this.name = name;
+    public Peasant() {
+    	// no-argument constructor
     }
 
     public long getId() {
