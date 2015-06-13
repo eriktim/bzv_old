e638f89c57d8006b27ae2be757cce171d31cb824
Erik Timmers <e.timmers@gmail.com>
2015-06-14 00:56:55 +0200

    angular-spring-data-rest in progress...

--- src/main/resources/public/js/app.js
+++ src/main/resources/public/js/app.js
@@ -1,46 +1,51 @@
 (function() {
 	
-var app = angular.module('bzvApp', []);
+var app = angular.module('bzvApp', ['ngResource', 'spring-data-rest']);
+
+app.config(function (SpringDataRestInterceptorProvider) {
+	SpringDataRestInterceptorProvider.apply();
+});
 
 app.controller('candidates', function($scope, $http) {
 	$http.get('/candidate').
-		success(function(data) {
-			$scope.candidates = data._embedded.candidate || [];
+		success(function(data) {console.log(data);
+console.log(data._embeddedItems[0]._resources('peasant').get(function(a){console.log(a);})); // FIXME
+			$scope.candidates = data._embeddedItems || [];
 		});
 });
 
 app.controller('peasants', function($scope, $http) {
 	$http.get('/peasant').
 		success(function(data) {
-			$scope.peasants = data._embedded.peasant || [];
+			$scope.peasants = data._embeddedItems || [];
 		});
 });
 
 app.controller('votes', function($scope, $http) {
 	$http.get('/vote').
 		success(function(data) {
-			$scope.votes = data._embedded.vote || [];
+			$scope.votes = data._embeddedItems || [];
 		});
 });
 
 app.controller('votePeriods', function($scope, $http) {
 	$http.get('/voteperiod').
 		success(function(data) {
-			$scope.votePeriods = data._embedded.voteperiod || [];
+			$scope.votePeriods = data._embeddedItems || [];
 		});
 });
 
 app.controller('voteTypes', function($scope, $http) {
 	$http.get('/votetype').
 		success(function(data) {
-			$scope.voteTypes = data._embedded.votetype || [];
+			$scope.voteTypes = data._embeddedItems || [];
 		});
 });
 
 app.controller('users', function($scope, $http) {
 	$http.get('/user').
 		success(function(data) {
-			$scope.users = data._embedded.user || [];
+			$scope.users = data._embeddedItems || [];
 		});
 });
 
