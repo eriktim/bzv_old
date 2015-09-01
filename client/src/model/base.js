7143b1d304a984233a0419c3f95f858048112644
Erik Timmers <e.timmers@gmail.com>
2015-09-01 21:39:30 +0200

    cors xr

--- client/src/model/base.js
+++ client/src/model/base.js
@@ -1,8 +1,21 @@
-//import xr from '../lib/xr/xr.js';
+import xr from '../../lib/xr/xr.js';
+
+var host = 'http://localhost:8080/';
+var mType = Symbol();
+var mId = Symbol();
 
 class Base {
   constructor(type, id) {
-    console.log('TODO load ' + type + '/' + id);
+    this[mType] = type;
+    this[mId] = id;
+    this.load();
+  }
+
+  load() {
+    xr.get(host + this[mType] + '/' + this[mId])
+      .then(function(data) {
+        console.log(data);
+      });
   }
 }
 
--- client/src/model/peasant.js
+++ client/src/model/peasant.js
@@ -1,11 +1,8 @@
 import Base from 'model/base.js';
 
-var mId = Symbol();
-
 class Peasant extends Base {
   constructor(id) {
     super('peasant', id);
-    this[mId] = id;
   }
 }
 
